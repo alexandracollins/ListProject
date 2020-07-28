088cfb81b59c55e55b472c13f504e2a07e6e2a25
Alexandra Collins <alexandracollins2022@gmail.com>
2020-07-28 10:21:12 -0500

    Initial Commit

---
+++ ListProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListProject
+//
+//  Created by Ivette Collins on 7/28/20.
+//  Copyright © 2020 AlexandraCollins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
